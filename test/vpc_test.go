a7b7af25643f0a85e5098c5471a88f00464b20a0
Catherine Vee <catherine.vee@outlook.com>
2025-09-15 09:36:28 -0700

    Fix VPC test: use single NAT Gateway to reduce EIP requirement and avoid limit

--- test/vpc_test.go
+++ test/vpc_test.go
@@ -89,7 +89,7 @@ terraform {
 			"private_subnet_cidrs": []string{"10.0.1.0/24", "10.0.2.0/24"},
 			"public_subnet_cidrs":  []string{"10.0.101.0/24", "10.0.102.0/24"},
 			"enable_nat_gateway":   true,
-			"single_nat_gateway":   false,
+			"single_nat_gateway":   true,
 			"enable_flow_log":      true,
 			"common_tags": map[string]string{
 				"Environment": "test",
